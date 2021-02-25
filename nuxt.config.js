e90dc29d64b37c29ba2aceb0fa60b1a26a3a9ee6
Alexandre Soares <alexandresoares@Alexandres-MacBook-Pro.local>
2021-02-25 14:51:55 +0200

    adding favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,13 +12,45 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
+      {
+        property: 'og:title',
+        content: 'Alexandre SOARES - VueJS Web Developer',
+      },
+      {
+        property: 'og:description',
+        content: `VueJS Web Developer - My name is Alexandre SOARES and I'm a VueJS Web Developer!`,
+      },
+      {
+        property: 'og:image',
+        content: `/featured-image.png`,
+      },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       {
         rel: 'stylesheet',
         href:
           'https://cdn.jsdelivr.net/npm/bootstrap-4-grid@3/css/grid.min.css',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/favicon/apple-touch-icon.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '180x180',
+        href: '/favicon/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon/favicon-16x16.png',
+      },
+      {
+        rel: 'manifest',
+        href: '/favicon/site.webmanifest',
       },
     ],
   },
